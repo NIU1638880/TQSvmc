9441a901888c01dd254391429ce7f78b08f4e24f
Bilal <1638880@uab.cat>
2023-10-03 13:57:34 +0200

    0.1

---
+++ src/vista/printer.js
@@ -0,0 +1,8 @@
+
+function pintarCelda(fila, columna) {
+    var cell = document.getElementById('cell-' + fila + '-' + columna);
+    if (cell) {
+        cell.classList.add('pintada');
+    }
+}
+
---
+++ src/vista/tablero.js
@@ -0,0 +1,19 @@
+
+    // Obtén una referencia a la tabla
+var table = document.querySelector('table');
+
+    // Número de filas y columnas en la matriz
+    var numRows = 10;
+    var numCols = 10;
+
+    // Genera las filas y celdas utilizando un bucle
+    for (var i = 1; i <= numRows; i++) {
+        var row = document.createElement('tr');
+        for (var j = 1; j <= numCols; j++) {
+            var cell = document.createElement('td');
+            cell.textContent = i + ',' + j; // Contenido de la celda
+            cell.id = 'cell-' + i + '-' + j; // ID de la celda
+            row.appendChild(cell);
+        }
+        table.appendChild(row);
+    }
