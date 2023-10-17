fd7fe1ea59eabe101ee91f3df3253332497f7cb7
Bilal <1638880@uab.cat>
2023-10-18 00:06:04 +0200

    updates

--- src/vista/printer.js
+++ src/vista/printer.js
@@ -1,6 +1,32 @@
 function pintarCelda(fila, columna) {
     var cell = document.getElementById('cell-' + fila + '-' + columna);
     if (cell) {
-        cell.classList.add('pintada');
+        cell.style.backgroundColor = "blue"; // Cambia el color a azul u otro color de tu elección
+    } else {
+        console.log('La celda no existe');
     }
+}
+
+let barcos = [];
+
+function guardarBarco(numeroBarco) {
+    let formId = "form" + numeroBarco;
+    let form = document.getElementById(formId);
+    let piece1 = form.elements["piece1"].value;
+    let piece2 = form.elements["piece2"].value;
+
+    // Agregar lógica para validar las coordenadas
+
+    // Lógica para guardar en el array de barcos
+    barcos.push({numeroBarco, piece1, piece2});
+
+
+    // Ejemplo de cómo pintar las celdas individualmente
+    let coordenadas1 = piece1.split(',');
+    let coordenadas2 = piece2.split(',');
+    pintarCelda(coordenadas1[0], coordenadas1[1]);
+    pintarCelda(coordenadas2[0], coordenadas2[1]);
+
+    // Comprovacion de que se guardan los barcos
+    console.log(barcos);
 }
