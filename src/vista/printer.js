82cd2a8f6f7c8508c51b82c0f9629899bca34cc4
Bilal <1638880@uab.cat>
2023-10-29 19:39:46 +0100

    up

--- src/vista/printer.js
+++ src/vista/printer.js
@@ -12,21 +12,35 @@ let barcos = [];
 function guardarBarco(numeroBarco) {
     let formId = "form" + numeroBarco;
     let form = document.getElementById(formId);
-    let piece1 = form.elements["piece1"].value;
-    let piece2 = form.elements["piece2"].value;
 
-    // Agregar lógica para validar las coordenadas
+    let piece1 = convertirCoordenadas(form.elements["piece1"].value);
+    let piece2 = convertirCoordenadas(form.elements["piece2"].value);
 
-    // Lógica para guardar en el array de barcos
-    barcos.push({numeroBarco, piece1, piece2});
+    let barco = {
+        numeroBarco,
+        piece1,
+        piece2
+    };
 
+    if (coordenadasValidas(barco, barcos, numRows, numCols)) {
+        // Lógica para guardar en el array de barcos
+        barcos.push(barco);
 
-    // Ejemplo de cómo pintar las celdas individualmente
-    let coordenadas1 = piece1.split(',');
-    let coordenadas2 = piece2.split(',');
-    pintarCelda(coordenadas1[0], coordenadas1[1]);
-    pintarCelda(coordenadas2[0], coordenadas2[1]);
+        // Ejemplo de cómo pintar las celdas individualmente
+        pintarCelda(piece1.x, piece1.y);
+        pintarCelda(piece2.x, piece2.y);
 
-    // Comprovacion de que se guardan los barcos
-    console.log(barcos);
+        // Comprobación de que se guardan los barcos
+        console.log(barcos);
+    } else {
+        console.log('Coordenadas inválidas o ya ocupadas.');
+    }
+}
+
+function convertirCoordenadas(coordenadaStr) {
+    let partes = coordenadaStr.split(',');
+    return {
+        x: parseInt(partes[0], 10),
+        y: parseInt(partes[1], 10)
+    };
 }
